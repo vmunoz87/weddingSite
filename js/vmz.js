359c63c6918dcd4b5a16b2094f5a8e2c601112d1
VMZ <31179007+vmunoz87@users.noreply.github.com>
2017-11-17 21:24:25 +0100

    2017-11-17: CountDown function adding

--- js/vmz.js
+++ js/vmz.js
@@ -1,4 +1,33 @@
-$(document).ready(function() { // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered 
+function countDown() { 
+	// Set the date we're counting down to
+	var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+	
+	// Update the count down every 1 second
+	var x = setInterval(
+			
+			function() {
 
-//closing ready function
-})
+						  // Get todays date and time
+						  var now = new Date().getTime();
+					
+						  // Find the distance between now an the count down date
+						  var distance = countDownDate - now;
+					
+						  // Time calculations for days, hours, minutes and seconds
+						  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+						  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+						  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+						  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+					
+						  // Display the result in the element with id="countDown"
+						  document.getElementById("countDown").innerHTML = days + "d " + hours + "h "
+						  + minutes + "m " + seconds + "s ";
+					
+						  // If the count down is finished, write some text 
+						  if (distance < 0) {
+							    clearInterval(x);
+							    document.getElementById("countDown").innerHTML = "EXPIRED";
+						  }
+						}, 1000);
+	
+};
